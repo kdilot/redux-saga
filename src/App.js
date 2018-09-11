56e12a0d5889d6b5e5b1b93cd99e3d9897961afb
kdilot <kdilot@gmail.com>
2018-09-10 21:49:26 -0700

    default

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './containers/Button';
+import View from './containers/View'
+import Loading from './containers/Loading'
+let App = () => (
+  <div>
+     <Button />
+     <Loading />
+     <View />
+  </div>
+);
+export default App;
